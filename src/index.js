251ab03eecf7911c4e0dd9ba1638b415a140836a
NSorokovaya <nadiaasorokovaya@gmail.com>
2023-02-06 07:39:58 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,31 +9,32 @@ const gallery = document.querySelector('.gallery');
 const btnLoadMore = document.querySelector('.load-more');
 let gallerySimpleLightbox = new SimpleLightbox('.gallery a');
 
-
 btnLoadMore.style.display = 'none';
 
 let pageNumber = 1;
+let maxPage = 1;
 
 btnSearch.addEventListener('click', e => {
   e.preventDefault();
   cleanGallery();
   const trimmedValue = input.value.trim();
-  if (trimmedValue !== '') {
-    fetchImages(trimmedValue, pageNumber).then(foundData => {
-      if (foundData.hits.length === 0) {
-        Notiflix.Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      } else {
-        renderImageList(foundData.hits);
-        Notiflix.Notify.success(
-          `Hooray! We found ${foundData.totalHits} images.`
-        );
-        btnLoadMore.style.display = 'block';
-        gallerySimpleLightbox.refresh();
-      }
-    });
-  }
+  if (!trimmedValue) return;
+  fetchImages(trimmedValue, pageNumber).then(foundData => {
+    if (foundData.hits.length === 0) {
+      return Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+
+    renderImageList(foundData.hits);
+    Notiflix.Notify.success(`Hooray! We found ${foundData.totalHits} images.`);
+    btnLoadMore.style.display = 'block';
+    gallerySimpleLightbox.refresh();
+
+    if (foundData.hits.length < 12) {
+      btnLoadMore.style.display = 'none';
+    }
+  });
 });
 
 btnLoadMore.addEventListener('click', () => {
